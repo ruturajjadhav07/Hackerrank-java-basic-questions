12cb0227bbf1c4876744520f5eea1ec668bd5aa2
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2024-01-04 11:39:49 +0530

    Create Solution.java

---
+++ Java Stdin and Stdout II/Solution.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        double b = sc.nextDouble();
+        sc.nextLine();
+        String c = sc.nextLine();
+        
+        System.out.println("String: "+c);
+        System.out.println("Double: "+b);
+        System.out.println("Int: "+a);
+        
+    }
+}
