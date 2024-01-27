9bb1d6af935f93361541cf62c50d681b52993d21
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2024-01-27 22:04:07 +0530

    Update Solution.java

--- Java Substring Comparisons/Solution.java
+++ Java Substring Comparisons/Solution.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
 
+public class Solution {
+
+public static String getSmallestAndLargest(String s, int k) {
+    String smallest = "";
+    String largest = "";
+
+    java.util.ArrayList<String> substrings = new java.util.ArrayList<>();      
+    for(int start=0;start<=s.length()-k;start++)
+    {
+        int end =start+k;
+        String substring =s.substring(start,end);
+        substrings.add(substring);            
+    }
+   smallest = java.util.Collections.min(substrings);
+   largest =java.util.Collections.max(substrings);
+   return smallest + "\n" + largest;
+}
+
+
+public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String s = scan.next();
+    int k = scan.nextInt();
+    scan.close();
+
+    System.out.println(getSmallestAndLargest(s, k));
+}
+}
